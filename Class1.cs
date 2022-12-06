30d33a5b97766487003e87c74cfe634b9cd764de
malakai gunderson <kaifungaming@gmail.com>
2022-12-06 15:01:23 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Essentials;
+using System.Runtime.InteropServices;
+namespace GetOsinfo
+{
+    public class Osinfo
+    {
+        public static bool OSInfo(string os)
+        {
+            var platform = DeviceInfo.Platform;
+            if (platform == DevicePlatform.Android && (os == "Android" || os == "Mobile"))
+            {
+                return true;
+            }
+            else if (platform == DevicePlatform.iOS && (os == "iOS" || os == "Mobile"))
+            {
+                return true;
+            }
+            else if (platform == DevicePlatform.Unknown)
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && (os == "Pc" || os == "Windows"))
+                {
+                    return true;
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && (os == "Pc" || os == "Linux"))
+                {
+                    return true;
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) && (os == "Pc" || os == "MacOS"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
